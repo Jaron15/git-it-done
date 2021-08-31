61e440e853e99a9c45c03cad45140841f0e8be56
Jaron <jjkenyon777@gmail.com>
2021-08-30 18:51:57 -0700

    add more results message

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,17 +1,26 @@
 var issueContinerEl = document.querySelector("#issues-container");
+var repoNameEl = document.querySelector("#repo-name")
+var limitWarningEl = document.querySelector("#limit-warning");
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    
     fetch(apiUrl).then(function(response) {
-        // request was successful
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+    
+                // check if api has paginated issues 
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
+            console.log(response);
             alert("there was a problem with your request!");
-        }
+        };
     });
 };
 var displayIssues = function(issues) {
@@ -47,7 +56,18 @@ var displayIssues = function(issues) {
         issueEl.appendChild(typeEl);
         issueContinerEl.appendChild(issueEl);
     }
-    
+};
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container 
+    limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
